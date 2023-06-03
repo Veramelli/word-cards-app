7aa901e53d0baa0dc88992eeb7f94844187aedb1
Vera Melnikova <veralitv88@mail.ru>
2023-06-03 17:00:44 +0200

    add: word counter and word change

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,23 +1,43 @@
 import React, { useState } from "react";
 import "./_Card.scss";
 
-function Card() {
+function Card(props) {
+  console.log(props.word.length);
   const [pressed, setPressed] = useState(false);
+  const [idWord, setIdWord] = useState(0);
+  const word = props.word[idWord];
 
   function showTranslation() {
     setPressed((prev) => !prev);
   }
 
+  function showPrevWord() {
+    setIdWord(idWord - 1);
+  }
+
+  function showNextWord() {
+    setIdWord(idWord + 1);
+  }
+
   return (
     <div className="card">
       <div className="card__wrapper">
-        <div className="card__word">move</div>
-        <div className="card__transcription">[ muːv ]</div>
+        <div className="card__word">{word.english}</div>
+
+        <div className="card__transcription">{word.transcription}</div>
+
         {pressed ? (
-          <div className="card__translation">двигать</div>
+          <div className="card__translation">{word.russian}</div>
         ) : (
           <button onClick={showTranslation}>Показать перевод</button>
         )}
+        <div>
+          {idWord + 1} / {props.word.length}
+        </div>
+      </div>
+      <div className="button__wrapper">
+        <button className="button-left" onClick={showPrevWord}></button>
+        <button className="button-right" onClick={showNextWord}></button>
       </div>
     </div>
   );
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import Card from "../Card/Card";
 import WordList from "../WordList/WordList";
 import "./_Main.scss";
+import words from "../words.json";
 
 export default function Main() {
   return (
     <div className="main">
-      <Card />
+      <Card word={words} />
       <WordList />
     </div>
   );
